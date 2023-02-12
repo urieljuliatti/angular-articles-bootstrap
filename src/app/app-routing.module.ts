03bd91e68a0c733e2d505f833c460a633b49e3fc
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2023-02-12 11:31:02 -0300

    Add Bootstrap icons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateArticleComponent } from './components/articles/create-article/create-article.component';
+import { ListArticlesComponent } from './components/articles/list-articles/list-articles.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'createArticle',
+    component: CreateArticleComponent
+  },
+  {
+    path: 'listArticles',
+    component: ListArticlesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CreateArticleComponent } from './components/articles/create-article/create-article.component';
 import { FormsModule } from '@angular/forms';
+import { ListArticlesComponent } from './components/articles/list-articles/list-articles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    CreateArticleComponent
+    CreateArticleComponent,
+    ListArticlesComponent
   ],
   imports: [
     BrowserModule,
