5e56a63b82e78187434a5764d5b2995da2df48bb
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2023-02-12 09:24:23 -0300

    Create new article form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CreateArticleComponent } from './components/articles/create-article/create-article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CreateArticleComponent
   ],
   imports: [
     BrowserModule,
