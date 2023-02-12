a6faa38161266b1eb230d5dbdb74f8aa73b053b0
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2023-02-12 12:47:38 -0300

    NgFor e comunicação entre componentes

--- src/app/components/articles/article/article.component.ts
+++ src/app/components/articles/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-article',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent {
+
+  @Input() article = {
+    title: '',
+    body: ''
+  }
 
 }
--- src/app/components/articles/list-articles/list-articles.component.ts
+++ src/app/components/articles/list-articles/list-articles.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-articles.component.css']
 })
 export class ListArticlesComponent {
-
+  articlesList = [
+    {
+      title: 'My Title',
+      body: "My Body"
+    },
+    {
+      title: 'My Title 2',
+      body: "My Body 2"
+    },
+    {
+      title: 'My Title 3',
+      body: "My Body 3"
+    }
+  ]
 }
