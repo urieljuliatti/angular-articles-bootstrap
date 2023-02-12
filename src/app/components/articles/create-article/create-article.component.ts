8cd169f39c8087f175573fa9ef3f6a85bf5e172d
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2023-02-12 15:12:19 -0300

    Creating article and preparing modal

--- src/app/components/articles/create-article/create-article.component.ts
+++ src/app/components/articles/create-article/create-article.component.ts
@@ -1,21 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Article } from '../article';
+import { ArticleService } from '../article.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-article',
   templateUrl: './create-article.component.html',
   styleUrls: ['./create-article.component.css']
 })
-export class CreateArticleComponent {
+export class CreateArticleComponent implements OnInit{
   article: Article = {
-    id: 0,
-    title: 'My Article',
-    body: 'My body'
+    title: '',
+    body: ''
   }
+
+  constructor (
+    private service: ArticleService,
+    private router: Router
+    ) { }
 
   onSubmit()
   {
-    alert( 'Create Article')
+    this.service.create(this.article).subscribe(() => {
+      this.router.navigate(['listArticles'])
+    })
   }
 
+  ngOnInit(): void {
+
+
+  }
 }
